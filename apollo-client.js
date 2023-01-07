201178e7348a602a72aa1784a3b1192bb70044e1
Erick Sáenz <ericksaenz@outlook.com>
2023-01-07 16:18:32 -0500

    Update apollo-client.js

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://31.220.52.62:1350/graphql",
+    uri: "http://192.101.68.28:1350/graphql",
     cache: new InMemoryCache(),
 });
 
